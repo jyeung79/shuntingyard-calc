5a34f29bb7658ada3c661f9616a3c0d9b8e1c3e4
Jeffery <zjyeung@sfu.ca>
2020-02-13 17:26:50 -0800

    Implement in electron js

---
+++ main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow } = require('electron')
+
+function createWindow () {
+	// Create the browser windows
+	let mainWindow = new BrowserWindow({
+		width: 500,
+		height: 580,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	})
+	mainWindow.setMenuBarVisibility(false)
+
+	// and load the index.html of the app
+	mainWindow.loadFile('./static/index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quite()
+	}
+})
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow()
+	}
+})
